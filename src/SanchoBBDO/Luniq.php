0a9049c617c73dfa5278ece4920a2ac707eee831
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-02-26 10:17:54 -0500

    init luniq of method with basic encryption logic

--- src/SanchoBBDO/Luniq.php
+++ src/SanchoBBDO/Luniq.php
@@ -4,5 +4,13 @@ namespace SanchoBBDO;
 
 class Luniq
 {
-
+    public function of($index)
+    {
+        $index = base_convert($index, 10, 36);
+        $index = str_pad($index, 4, '0', STR_PAD_LEFT);
+        $encoded = hash_hmac('sha1', $index, "secret key", true);
+        $encoded = trim(base64_encode($encoded));
+        return $index.substr($encoded, 0, 6);
+    }
 }
+
