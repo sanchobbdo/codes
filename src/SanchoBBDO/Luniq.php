2766770f82354928057fb12e15c9bca293bf5940
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-02-26 14:40:36 -0500

    use sha1 to encode message

--- src/SanchoBBDO/Luniq.php
+++ src/SanchoBBDO/Luniq.php
@@ -27,8 +27,7 @@ class Luniq
     {
         $index = base_convert($index, 10, 36);
         $index = str_pad($index, 4, '0', STR_PAD_LEFT);
-        $encoded = hash_hmac('sha1', $index, $this->getSecretKey(), true);
-        $encoded = trim(base64_encode($encoded));
+        $encoded = sha1($index.$this->getSecretKey());
         return $index.substr($encoded, 0, 6);
     }
 }
