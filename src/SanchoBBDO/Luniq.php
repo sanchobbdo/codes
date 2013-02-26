3a867e22f6285ca2e4d4dd4225503bc0de6b90a7
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-02-26 08:58:41 -0500

    init src and tests

---
+++ src/SanchoBBDO/Luniq.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SanchoBBDO;
+
+class Luniq
+{
+
+}
---
+++ tests/SanchoBBDO/Tests/Luniq/LuniqTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SanchoBBDO\Tests\Luniq;
+
+class LuniqTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        new \SanchoBBDO\Luniq();
+    }
+}
