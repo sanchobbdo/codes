b0ce7849b5e950859fb998f9da2aa507f48b96c1
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-02-26 11:03:10 -0500

    luniq contructor sets secretKey and length

--- src/SanchoBBDO/Luniq.php
+++ src/SanchoBBDO/Luniq.php
@@ -4,11 +4,30 @@ namespace SanchoBBDO;
 
 class Luniq
 {
+    protected $secretKey;
+    protected $length;
+
+    public function __construct($config)
+    {
+        $this->secretKey = $config['secretKey'];
+        $this->length = isset($config['length']) ? $config['length'] : 1679616;
+    }
+
+    public function getSecretKey()
+    {
+        return $this->secretKey;
+    }
+
+    public function getLength()
+    {
+        return $this->length;
+    }
+
     public function of($index)
     {
         $index = base_convert($index, 10, 36);
         $index = str_pad($index, 4, '0', STR_PAD_LEFT);
-        $encoded = hash_hmac('sha1', $index, "secret key", true);
+        $encoded = hash_hmac('sha1', $index, $this->getSecretKey(), true);
         $encoded = trim(base64_encode($encoded));
         return $index.substr($encoded, 0, 6);
     }
--- tests/SanchoBBDO/Tests/LuniqTest.php
+++ tests/SanchoBBDO/Tests/LuniqTest.php
@@ -6,7 +6,20 @@ class LuniqTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->luniq = new \SanchoBBDO\Luniq();
+        $this->luniq = new \SanchoBBDO\Luniq(array(
+            'secretKey' => 'my secret key'
+        ));
+    }
+
+    public function testConstructorSetsConfig()
+    {
+        $luniq = new \SanchoBBDO\Luniq(array(
+            'secretKey' => 'some secret key',
+            'length' => 1000
+        ));
+
+        $this->assertEquals('some secret key', $luniq->getSecretKey());
+        $this->assertEquals(1000, $luniq->getLength());
     }
 
     public function testOfReturnsAString()
