9b03701ae9a07cdcad6c87162289bbad65be5cc8
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-02-28 09:00:43 -0500

    dump command secret-key option definition

---
+++ src/SanchoBBDO/Codes/Command/DumpCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SanchoBBDO\Codes\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DumpCommand extends Command
+{
+    public function configure()
+    {
+        $this
+            ->setName('dump')
+            ->setDescription('Dumps codes to screen')
+            ->addOption(
+                'secret-key',
+                'k',
+                InputOption::VALUE_REQUIRED,
+                'Secret key to generate codes'
+            );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
---
+++ tests/SanchoBBDO/Tests/Codes/Command/DumpCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SanchoBBDO\Tests\Codes\Command;
+
+use SanchoBBDO\Codes\Command\DumpCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class DumpCommandTest extends \PHPUnit_Framework_TestCase
+{
+    protected function executeCommand($params = array())
+    {
+        $this->commandTester->execute(array_merge(
+            array('command' => $command->getName()),
+            $params
+        ));
+    }
+
+    public function setUp()
+    {
+        $application = new Application();
+        $application->add(new DumpCommand);
+
+        $this->command = $application->find('dump');
+        $this->definition = $this->command->getDefinition();
+
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    public function testSecretKeyOption()
+    {
+        try {
+            $option = $this->definition->getOption('secret-key');
+            $this->assertEquals('k', $option->getShortcut());
+            $this->assertTrue($option->isValueRequired());
+        } catch (\Exception $e) {
+            $this->fail("Option secret-key is not set");
+        }
+    }
+}
