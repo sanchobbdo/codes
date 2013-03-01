089b7d7ba99e2d0bb31db0e4953c4aa7fdd376b4
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-03-01 15:40:15 -0500

    create dump text command

---
+++ src/SanchoBBDO/Codes/Command/DumpTextCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SanchoBBDO\Codes\Command;
+
+use SanchoBBDO\Codes\Command\AbstractDumpCommand;
+use SanchoBBDO\Codes\DumpWriter\TextDumpWriter;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DumpTextCommand extends AbstractDumpCommand
+{
+    protected $action = 'txt';
+
+    public function configure()
+    {
+        parent::configure();
+
+        $this->addArgument(
+            'file',
+            InputArgument::REQUIRED,
+            'Text file to dump codes to'
+        );
+    }
+
+    public function getDumpWriter()
+    {
+        return new TextDumpWriter($this->getInput()->getArgument('file'));
+    }
+}
---
+++ tests/SanchoBBDO/Tests/Codes/Command/DumpTextCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SanchoBBDO\Tests\Codes\Command;
+
+use org\bovigo\vfs\vfsStream;
+use SanchoBBDO\Codes\Command\DumpTextCommand;
+
+class DumpTextCommandTest extends CommandTestCase
+{
+    protected function createCommand() {
+        return new DumpTextCommand;
+    }
+
+    public function testActionIsTxt()
+    {
+        $this->assertEquals('txt', $this->command->getAction());
+    }
+
+    public function testFileArgument()
+    {
+        $this->assertInputArgument('file', true);
+    }
+
+    public function testDumpsFileInGivenPath()
+    {
+        $root = vfsStream::setup('dump');
+        $file = 'dump.txt';
+        $filePath = vfsStream::url("dump/{$file}");
+
+        $this->executeCommand(array(
+            '--secret-key' => 'dum',
+            '--length' => 10,
+            'file' => $filePath
+        ));
+
+        $this->assertNotEmpty(file_get_contents($filePath));
+    }
+}
