bbeac60082a2046cda8e9e6d33618d2596974ebf
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-03-07 10:28:23 -0500

    add __construct method to coder interface

--- src/SanchoBBDO/Codes/Coder/CoderInterface.php
+++ src/SanchoBBDO/Codes/Coder/CoderInterface.php
@@ -6,6 +6,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 interface CoderInterface extends ConfigurationInterface
 {
+    public function __construct($config = array());
     public function encode($digit);
     public function isValid($code);
 }
