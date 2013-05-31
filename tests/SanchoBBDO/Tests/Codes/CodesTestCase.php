7c4c4486648c40724afc4576b25fc4da4390045f
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2013-05-31 16:53:14 -0500

    use mock coder in codes test suite

--- tests/SanchoBBDO/Tests/Codes/CodesTestCase.php
+++ tests/SanchoBBDO/Tests/Codes/CodesTestCase.php
@@ -2,16 +2,14 @@
 
 namespace SanchoBBDO\Tests\Codes;
 
-use SanchoBBDO\Codes\Coder\Coder;
+use SanchoBBDO\Tests\Codes\Coder\MockCoder;
 use SanchoBBDO\Codes\Codes;
 
 class CodesTestCase extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->coder = new Coder(array(
-            'secret_key' => '1461932c2e74b726c795742e1caa8b4a281ea09c'
-        ));
+        $this->coder = new MockCoder();
         $this->codes = new Codes($this->coder, 10, 10);
     }
 }
